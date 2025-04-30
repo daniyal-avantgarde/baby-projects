1eb6252feae68a0dbbe5e9a2ee785e68ed35c1ce
Daniyal <daniyal.avantgarde@gmail.com>
2025-04-30 13:35:34 +1000

    Removed BeautifulSoup4 library, added printing to text file functionality, and started cleaning up code.
    The script is functional at this point. I've tested it on a couple of websites. I've began cleaning up the code; I added a header comment explaining the functionality. There's still some comments at the bottom that need to be sorted.

--- website_email_scraper/email_scraper.py
+++ website_email_scraper/email_scraper.py
@@ -1,38 +1,63 @@
-# input website url
-# access website content (web scraping)
-# find emails and collect in list (pattern matching)
-# print list to csv (writing files)
+"""
+Pass a website url to this program via the command line and
+it will scrape all the emails from that website.
 
-import sys, webbrowser, requests, bs4, re
+The program executes these steps:
+1. Accepts website url as command line input 
+2. Accesses url and scrapes HTML
+3. Using regex, finds all emails in HTML and stores as them in a list 
+4. Prints the list of emails to a text file (.txt)
+"""
 
-# INPUT WEBSITE URL
+# STEP 1: INPUT WEBSITE URL #
+import sys
 url = sys.argv[1]
 
-# ACCESS WEBSITE CONTENT
+# STEP 2: ACCESS WEBSITE CONTENT #
+import requests
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
-} # apparently, have to use headers to avoid bot-detection
-# what are headers?
-response = requests.get(url, headers=headers) # downloads site content
-response.raise_for_status() # does nothing if all well, raises error if otherwise
-# print(f'TYPE: {type(page)}; LENGTH: {len(page.text)}')
-# page.text is the raw website html
+} 
+response = requests.get(url, headers=headers) 
+response.raise_for_status()
+html = response.text
 
-html = bs4.BeautifulSoup(response.text)
-
-# FIND EMAILS AND COLLECT IN LIST
-print(html.prettify)
+# STEP 3: FIND EMAILS AND COLLECT IN LIST #
+import re
 email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
-emails = re.findall(email_pattern, html.prettify())
-print(emails)
+emails = re.findall(email_pattern, html)
+emails = list(dict.fromkeys(emails))
+emails = [email for email in emails if not (email[-3:]=="png" or email[-3:]=="jpg")]
 
-# get rid of duplicate emails
+# STEP 4: PRINT LIST TO FILE #
+outputFile = open('output.txt', "w", newline='')
+for email in emails:
+    outputFile.write(email + '\n')
 
 
-# webbrowser.open(url)
 
 # fixed variable names
 # using .text on the BeautifulSoup object returns only the visible text on the website
 # you have to use .prettify() to get back the HTML, formatted for readability
+
+# not going to export to csv because... there's no reason to.
+# a text file will work just as well. i have no cells.
+# i only have one email per line
+
+# picking up weirdly named images like 12-2019_about-ig_women-at-sprout@2x.jpg so have to update regex expression to exclude those
+
+# apparently, have to use headers to avoid bot-detection
+# what are headers?
+
+""" 
+notes 
+response = requests.get(url, headers=headers) # downloads site content
+response.raise_for_status() # does nothing if all well, raises error if otherwise"""
+
+'''count = 1
+for email in emails:
+    print(f'Email {count}: {email}')
+    count += 1'''
+
 
 
