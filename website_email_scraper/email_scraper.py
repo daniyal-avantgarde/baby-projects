1f59bd96deacfe22bd7dd4c7db3567e8fc935a4f
Daniyal <daniyal.avantgarde@gmail.com>
2025-04-30 12:08:00 +1000

    I just realised that I don't even need the  BeautifulSoup4 library - I can just use regex on response.text, where response is the Response object returned by requests.get()
    I've noted a couple of questions I have in the code comments. I'll answer them next time, and add the answers to the folder's READ_ME file.

--- website_email_scraper/email_scraper.py
+++ website_email_scraper/email_scraper.py
@@ -3,22 +3,36 @@
 # find emails and collect in list (pattern matching)
 # print list to csv (writing files)
 
-import sys, webbrowser, requests, bs4
+import sys, webbrowser, requests, bs4, re
 
+# INPUT WEBSITE URL
 url = sys.argv[1]
 
-wikipedia_page = requests.get(url) # downloads site content
-wikipedia_page.raise_for_status() # does nothing if all well, raises error if otherwise
-print(f'TYPE: {type(wikipedia_page)}; LENGTH: {len(wikipedia_page.text)}')
-# download_site.text is the raw website html
+# ACCESS WEBSITE CONTENT
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+} # apparently, have to use headers to avoid bot-detection
+# what are headers?
+response = requests.get(url, headers=headers) # downloads site content
+response.raise_for_status() # does nothing if all well, raises error if otherwise
+# print(f'TYPE: {type(page)}; LENGTH: {len(page.text)}')
+# page.text is the raw website html
 
-wikipedia_soup = bs4.BeautifulSoup(wikipedia_page.text)
+html = bs4.BeautifulSoup(response.text)
 
-search_bar = wikipedia_soup.select('div.search-container')[0]
-print(search_bar.attrs)
-print(search_bar.get('class'))
+# FIND EMAILS AND COLLECT IN LIST
+print(html.prettify)
+email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+emails = re.findall(email_pattern, html.prettify())
+print(emails)
+
+# get rid of duplicate emails
 
 
-webbrowser.open(url)
+# webbrowser.open(url)
+
+# fixed variable names
+# using .text on the BeautifulSoup object returns only the visible text on the website
+# you have to use .prettify() to get back the HTML, formatted for readability
 
 
